7be50b42e76f9c2b163da41c509ec94c48375a8c
Daniel Wieczorek <danielwieczorek901@gmail.com>
2023-07-30 19:16:18 +0200

    added functions in js

--- js/scirpt.js
+++ js/scirpt.js
@@ -1,7 +1,4 @@
 {
-    const selectCurrency = document.querySelector(".js-selectCurrency")
-    const resultElement = document.querySelector(".js-result")
-
     const welcome = () => {
         console.log("Hello Dev!");
     }
@@ -24,7 +21,15 @@
         };
     }
 
-    const resetResult = () => {
+    const updateResult = () => {
+        const selectCurrency = document.querySelector(".js-selectCurrency")
+        const resultElement = document.querySelector(".js-result")
+        const amountElement = document.querySelector(".js-amount")
+        const amount = amountElement.value;
+        const currency = selectCurrency.value;
+        const result = calculateResult(amount, currency);
+        resultElement.innerHTML = ` ${result.toFixed(2)} ${currency}`;
+
         selectCurrency.addEventListener("change", () => {
             resultElement.innerHTML = "";
         })
@@ -36,20 +41,11 @@
         updateResult();
     };
 
-    const updateResult = () => {
-        const amountElement = document.querySelector(".js-amount")
-        const amount = amountElement.value;
-        const currency = selectCurrency.value;
-        const result = calculateResult(amount, currency);
-        resultElement.innerHTML = ` ${result.toFixed(2)} ${currency}`;
-    }
-
     const init = () => {
         const formElement = document.querySelector(".js-form")
         formElement.addEventListener("submit", onFormSubmit);
 
         welcome();
-        resetResult();
     }
 
     init();
