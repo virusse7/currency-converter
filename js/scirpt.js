558672b42818296086f1049ab92356791917651c
Daniel Wieczorek <danielwieczorek901@gmail.com>
2023-08-01 00:01:51 +0200

    refractoring script.js, changed functions

--- js/scirpt.js
+++ js/scirpt.js
@@ -1,7 +1,7 @@
 {
     const welcome = () => {
         console.log("Hello Dev!");
-    }
+    };
 
     const calculateResult = (amount, currency) => {
         const rateEUR = 4.46;
@@ -19,34 +19,27 @@
                 return amount / rateARS;
 
         };
-    }
+    };
 
-    const updateResult = () => {
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
         const selectCurrency = document.querySelector(".js-selectCurrency")
         const resultElement = document.querySelector(".js-result")
         const amountElement = document.querySelector(".js-amount")
         const amount = amountElement.value;
         const currency = selectCurrency.value;
         const result = calculateResult(amount, currency);
+
         resultElement.innerHTML = ` ${result.toFixed(2)} ${currency}`;
-
-        selectCurrency.addEventListener("change", () => {
-            resultElement.innerHTML = "";
-        })
-    }
-
-    const onFormSubmit = (event) => {
-        event.preventDefault();
-        calculateResult();
-        updateResult();
     };
 
     const init = () => {
-        const formElement = document.querySelector(".js-form")
+        const formElement = document.querySelector(".js-form");
         formElement.addEventListener("submit", onFormSubmit);
 
         welcome();
-    }
+    };
 
     init();
 }
